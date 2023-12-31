916e8915c684683c808c459180d3b55dafa6c3c9
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-12-31 16:44:17 +0100

    feat: render page

---
+++ js/pages/home.js
@@ -0,0 +1,5 @@
+const page = document.createElement('div');
+page.innerHTML = 'Hello world';
+export default function renderHome() {
+  return page;
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+import renderHome from './js/pages/home';
+
+const app = document.querySelector('.app');
+
+app.appendChild(renderHome());
