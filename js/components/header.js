49fdae950527583144bb226f3677a76cf56a1a0d
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2024-01-02 18:42:36 +0100

    feat: add header background image

--- js/components/header.js
+++ js/components/header.js
@@ -3,11 +3,10 @@ import BookTableButton from './book-table-button';
 
 export default function Header(headingText, paragraphText, page = 'home') {
   const header = document.createElement('header');
-  header.className = `header`;
+  header.className = `header header--${page}`;
 
   const h1 = document.createElement('h1');
   h1.className = `header__heading`;
-  h1.style.backgroundColor = 'black'; // temp
 
   const img = document.createElement('img');
   img.src = websiteLogoURL;
