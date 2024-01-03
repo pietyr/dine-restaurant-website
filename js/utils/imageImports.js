7f612ab0f84c29167240c446f865290e4bd9b7fb
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2024-01-03 15:59:02 +0100

    feat: aggregate image imports

---
+++ js/utils/imageImports.js
@@ -0,0 +1,33 @@
+import beefDesktopTabletURL from '../../images/homepage/beef-desktop-tablet.jpg';
+import beefDesktopTablet2xURL from '../../images/homepage/beef-desktop-tablet@2x.jpg';
+import beefMobileURL from '../../images/homepage/beef-mobile.jpg';
+import beefMobile2xURL from '../../images/homepage/beef-mobile@2x.jpg';
+import chocolateDesktopTabletURL from '../../images/homepage/chocolate-desktop-tablet.jpg';
+import chocolateDesktopTablet2xURL from '../../images/homepage/chocolate-desktop-tablet@2x.jpg';
+import chocolateMobileURL from '../../images/homepage/chocolate-mobile.jpg';
+import chocolateMobile2xURL from '../../images/homepage/chocolate-mobile@2x.jpg';
+import salmonDesktopTabletURL from '../../images/homepage/salmon-desktop-tablet.jpg';
+import salmonDesktopTablet2xURL from '../../images/homepage/salmon-desktop-tablet@2x.jpg';
+import salmonMobileURL from '../../images/homepage/salmon-mobile.jpg';
+import salmonMobile2xURL from '../../images/homepage/salmon-mobile@2x.jpg';
+
+export default {
+  beef: {
+    desktop: beefDesktopTabletURL,
+    desktop2x: beefDesktopTablet2xURL,
+    mobile: beefMobileURL,
+    mobile2x: beefMobile2xURL,
+  },
+  chocolate: {
+    desktop: chocolateDesktopTabletURL,
+    desktop2x: chocolateDesktopTablet2xURL,
+    mobile: chocolateMobileURL,
+    mobile2x: chocolateMobile2xURL,
+  },
+  salmon: {
+    desktop: salmonDesktopTabletURL,
+    desktop2x: salmonDesktopTablet2xURL,
+    mobile: salmonMobileURL,
+    mobile2x: salmonMobile2xURL,
+  },
+};
