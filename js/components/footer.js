cbeba1cb01a5c1f54e08661dc8227c98dceb35b2
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2024-01-04 21:51:50 +0100

    feat: add classes to prefooter and footer

--- js/components/footer.js
+++ js/components/footer.js
@@ -2,17 +2,22 @@ import websiteLogoURL from '../../images/logo.svg';
 
 export default function Footer() {
   const footer = document.createElement('footer');
+  footer.className = 'footer';
 
   const logo = document.createElement('img');
   logo.src = websiteLogoURL;
+  logo.className = 'footer__logo';
 
   const adress = document.createElement('adress');
+  adress.className = 'footer__text-box';
 
   adress.appendChild(Span('marthwaite.sedbergh'));
   adress.appendChild(Span('cumbria'));
   adress.appendChild(Span('+00 44 123 4567'));
 
-  const openTimes = document.createElement('p');
+  const openTimes = document.createElement('div');
+  openTimes.className = 'footer__text-box';
+
   openTimes.appendChild(Span('open times'));
   openTimes.appendChild(Span('mon - fri: 09:00 am - 10:00 pm'));
   openTimes.appendChild(Span('sat - sun: 09:00 am - 11:30 pm'));
@@ -27,5 +32,6 @@ export default function Footer() {
 function Span(text) {
   const span = document.createElement('span');
   span.innerHTML = text;
+  span.className = 'footer__span';
   return span;
 }
--- js/components/prefooter.js
+++ js/components/prefooter.js
@@ -2,9 +2,11 @@ import Button from './button';
 
 export default function Prefooter() {
   const prefooter = document.createElement('section');
+  prefooter.className = 'prefooter';
 
   const paragraph = document.createElement('p');
   paragraph.innerHTML = 'Ready to make a reservation?';
+  paragraph.className = 'prefooter__text';
 
   prefooter.appendChild(paragraph);
   prefooter.appendChild(
