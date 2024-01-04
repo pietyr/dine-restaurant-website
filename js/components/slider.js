5180bff603a3e37945a3ea119b041566d06bf216
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2024-01-04 19:23:08 +0100

    feat: add classes to slider elements

--- js/components/slider.js
+++ js/components/slider.js
@@ -1,5 +1,7 @@
 import Button from './button';
 
+let currentSlide = 0;
+
 const content = [
   {
     title: 'Family Gathering',
@@ -17,30 +19,45 @@ const content = [
 
 export default function Slider() {
   const slider = document.createElement('section');
+  slider.className = 'slider';
 
-  content.forEach((slide) => slider.appendChild(Slide(slide)));
+  const slide = Slide(content[currentSlide]);
+  slider.appendChild(slide);
+
+  currentSlide++; // REMOVE THIS
+  // content.forEach((slide) => slider.appendChild(Slide(slide)));
 
   return slider;
 }
 
 function Slide({ title, text }) {
   const slide = document.createElement('article');
+  slide.className = 'slider__slide';
 
   const imgWrapper = document.createElement('div');
+  imgWrapper.className = 'slider__img-wrapper';
 
   const textWrapper = document.createElement('div');
+  textWrapper.className = 'slider__text-wrapper';
 
   const h2 = document.createElement('h2');
+  h2.className = 'slider__heading';
   h2.innerHTML = title;
 
   const paragraph = document.createElement('p');
+  paragraph.className = 'slider__text';
   paragraph.innerText = text;
 
   const list = document.createElement('ul');
+  list.className = 'slider__list';
 
-  content.forEach(({ title }) => {
+  content.forEach(({ title }, index) => {
     const li = document.createElement('li');
-    li.innerHTML = title;
+    li.className = 'slider__list-item';
+    if (index === currentSlide) {
+      li.classList.add('slider__list-item--active');
+    }
+    li.innerText = title;
     list.appendChild(li);
   });
 
